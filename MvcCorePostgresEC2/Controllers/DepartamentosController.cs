bbac427f159efca859a8f6e673ed59aa624a1172
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-05-13 13:13:45 +0200

    Add project files.

---
+++ MvcCorePostgresEC2/Controllers/DepartamentosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePostgresEC2.Models;
+using MvcCorePostgresEC2.Repositories;
+using System.Threading.Tasks;
+
+namespace MvcCorePostgresEC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> departamentos = await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Departamento dept = await this.repo.FindDepartamentoAsync(id);
+            return View(dept);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Departamento dept)
+        {
+            await this.repo.CreateDepartamentoAsync(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return RedirectToAction("Index");
+        }
+    }
+}
